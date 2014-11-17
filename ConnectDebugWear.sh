ae23f9776ebea7f8e16a8e1fbf476c9d6772c0ca
Johnny <j796160836@gmail.com>
2014-11-17 18:39:38 +0800

    updates ConnectDebugWear scripts that can choose the first devices to forward wear

--- ConnectDebugWear.sh
+++ ConnectDebugWear.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-adb forward tcp:4444 localabstract:/adb-hub; adb connect localhost:4444
+DEVICE=`adb devices | sed '/List of devices/d' | sed '/:4444/d' | head -n 1 | awk '{print $1 "\t"}'` 
+if [ "$DEVICE" == "" ]; then
+	echo "Please conntect devices!"
+	exit 0
+else 
+	echo "Devices:  "$DEVICE
+fi  
+adb -s $DEVICE forward tcp:4444 localabstract:/adb-hub; adb connect localhost:4444
